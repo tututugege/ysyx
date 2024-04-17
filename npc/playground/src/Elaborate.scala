1b107316e801c529593f7563fce69a113d1b3529
tututugege <903306512@qq.com>
2024-04-17 17:09:53 +0800

    test

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -2,6 +2,12 @@ import circt.stage._
 
 object Elaborate extends App {
   def top       = new TOP(32)
+  val useMFC    = false
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+  if (useMFC) {
+    (new ChiselStage)
+      .execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+  } else {
+    (new chisel3.stage.ChiselStage).execute(args, generator)
+  }
 }
